c17841e303f87e73ceabaf254e45cdb829b3b13f
Yupo <yupotown@gmail.com>
2015-10-18 22:32:39 +0900

    入力を1箇所にまとめ、出力のニューロンの辞書を作ってリファクタリング

--- FormalNeuron/Program.cs
+++ FormalNeuron/Program.cs
@@ -10,44 +10,49 @@ namespace NeuralNetwork
     {
         static void Main(string[] args)
         {
+            // input
+            var inputs = ConstOutput.CreateArray(false, false);
+
             // and
             var andNeuron = new FormalNeuron(2);
+            andNeuron.SetInputs(inputs);
             andNeuron.SetWeights(ConstOutput.CreateArray(1.0, 1.0));
             andNeuron.Threshold = ConstOutput.Create(1.5);
 
             // xor
-            var xorInput = ConstOutput.CreateArray(false, true);
-            var xorOutputNeuron = new FormalNeuron(2);
+            var xorNeuron = new FormalNeuron(2);
             var xorNeuron1 = new FormalNeuron(2);
-            var xorOutputNeuronThreshold = FunctionNeuron.Create((bool v) => v ? 2.5 : 0.5);
-            xorOutputNeuron.SetInputs(xorInput);
-            xorOutputNeuron.SetWeights(ConstOutput.CreateArray(1.0, 1.0));
-            xorOutputNeuron.Threshold = xorOutputNeuronThreshold;
-            xorOutputNeuronThreshold.Input = xorNeuron1;
-            xorNeuron1.SetInputs(xorInput);
+            var xorNeuronThreshold = FunctionNeuron.Create((bool v) => v ? 2.5 : 0.5);
+            xorNeuron.SetInputs(inputs);
+            xorNeuron.SetWeights(ConstOutput.CreateArray(1.0, 1.0));
+            xorNeuron.Threshold = xorNeuronThreshold;
+            xorNeuronThreshold.Input = xorNeuron1;
+            xorNeuron1.SetInputs(inputs);
             xorNeuron1.SetWeights(ConstOutput.CreateArray(1.0, 1.0));
             xorNeuron1.Threshold = ConstOutput.Create(1.5);
 
+            // names
+            var names = new Dictionary<string, ILazyOutput<bool>>();
+            names.Add("and", andNeuron);
+            names.Add("xor", xorNeuron);
+
             while (true)
             {
-                var input = Console.ReadLine().Split(' ').ToArray();
-                var name = input[0];
-                var prms = input.Skip(1).Select(v => v != "0").ToArray();
+                var line = Console.ReadLine().Split(' ').ToList();
+                var name = line[0];
+                var prms = line.Skip(1).Select(v => v != "0").ToList();
+                for (var i = 0; i < prms.Count; ++i)
+                {
+                    inputs[i].Set(prms[i]);
+                }
 
                 if (name == "quit")
                 {
                     break;
                 }
-                else if (name == "and")
+                else if (names.ContainsKey(name))
                 {
-                    andNeuron.SetInputs(ConstOutput.CreateArray(prms));
-                    Console.WriteLine(andNeuron.Output ? 1 : 0);
-                }
-                else if (name == "xor")
-                {
-                    xorInput[0].Set(prms[0]);
-                    xorInput[1].Set(prms[1]);
-                    Console.WriteLine(xorOutputNeuron.Output ? 1 : 0);
+                    Console.WriteLine(names[name].Output ? 1 : 0);
                 }
                 else
                 {
