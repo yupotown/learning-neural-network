2d33853f38fd2e1b1311b94a3f6fc70e58f1c14f
Yupo <yupotown@gmail.com>
2015-10-22 23:55:07 +0900

    BP の学習回数を増やしたらちゃんと動いた

--- HierarchicalNeuralNetwork/Program.cs
+++ HierarchicalNeuralNetwork/Program.cs
@@ -15,28 +15,21 @@ namespace HierarchicalNeuralNetwork
             Func<double, double> df = (x => f(x) * (1 - f(x)));
             var nn = new HierarchicalNetwork(2, 2, 1);
 
-            nn[1, 0].SetWeights(10.0, 10.0);
-            nn[1, 0].Threshold = -5.0;
-            nn[1, 1].SetWeights(10.0, 10.0);
-            nn[1, 1].Threshold = -15.0;
-            nn[2, 0].SetWeights(10.0, -10.0);
-            nn[2, 0].Threshold = -5.0;
-
-            //// 学習
-            //var bp = new Backpropagation(nn, df, 0.8, 0.75);
-            //var examples = new List<Tuple<double[], double[]>>()
-            //{
-            //    Tuple.Create(new double[]{0.0, 0.0}, new double[]{0.0}),
-            //    Tuple.Create(new double[]{0.0, 1.0}, new double[]{1.0}),
-            //    Tuple.Create(new double[]{1.0, 0.0}, new double[]{1.0}),
-            //    Tuple.Create(new double[]{1.0, 1.0}, new double[]{0.0}),
-            //};
-            //var rnd = new Random();
-            //for (var i = 0; i < 100; ++i)
-            //{
-            //    var ex = examples[rnd.Next(examples.Count)];
-            //    bp.Train(ex.Item1, ex.Item2);
-            //}
+            // 学習
+            var bp = new Backpropagation(nn, df, 0.8, 0.75);
+            var examples = new List<Tuple<double[], double[]>>()
+            {
+                Tuple.Create(new double[]{0.0, 0.0}, new double[]{0.0}),
+                Tuple.Create(new double[]{0.0, 1.0}, new double[]{1.0}),
+                Tuple.Create(new double[]{1.0, 0.0}, new double[]{1.0}),
+                Tuple.Create(new double[]{1.0, 1.0}, new double[]{0.0}),
+            };
+            var rnd = new Random();
+            for (var i = 0; i < 100000; ++i)
+            {
+                var ex = examples[rnd.Next(examples.Count)];
+                bp.Train(ex.Item1, ex.Item2);
+            }
 
             while (true)
             {
